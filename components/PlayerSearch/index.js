024cc73aad2b9a016630fda42906820d35bf42c6
David Haye <dkhaye@gmail.com>
2021-12-06 16:34:14 -0500

    initial tests

--- components/PlayerSearch/index.js
+++ components/PlayerSearch/index.js
@@ -3,114 +3,60 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import CircularProgress from "@mui/material/CircularProgress";
-import SeasonStats from 'components/SeasonStats';
+import SeasonStats from '@/components/SeasonStats';
 
-export default function PlayerSearch() {
+export default function PlayerSearch({players, onPlayerSelectHandle, onSearchChangeHandle}) {
   const [open, setOpen] = React.useState(false);
-  const [players, setPlayers] = React.useState([]);
-  const [stats, setStats] = React.useState([]);
   const loading = open && players.length === 0;
-
-  const onSearchChangeHandle = async value => {
-    if(value) {
-      const response = await fetch(
-        "https://suggest.svc.nhl.com/svc/suggest/v1/minplayers/" + value + "/10"
-      );
-
-      const players = await response.json();
-
-      setPlayers(players["suggestions"].map( str => {
-        let els = str.split("|");
-        let playerObj = {
-          name: els[2] + " " + els[1],
-          id: els[0]
-        };
-        return playerObj;
-      }));
-    } else {
-      setPlayers([]);
-    }
-  };
-
-  const onPlayerSelectHandle = async value => {
-    if (value && value.id) {
-      const response = await fetch(
-        "https://statsapi.web.nhl.com/api/v1/people/" + value.id + "/stats?stats=yearByYear"
-      );
-      const rawStats = await response.json();
-
-      if (!rawStats || !rawStats["stats"]) {
-        return {}
-      }
-
-      const nhlSeasons = rawStats["stats"][0]["splits"].filter( seasonHash => {
-        return seasonHash["league"]["name"] == "National Hockey League"
-      });
-
-      setStats(nhlSeasons.map( seasonHash => {
-        return {
-          year: seasonHash.season.substr(0,4) + " - " + seasonHash.season.substr(4,4),
-          team: seasonHash.team.name,
-          ...seasonHash.stat
-        }
-      }));
-    } else {
-      setStats([]);
-    }
-  };
 
   React.useEffect(() => {
     if (!open) {
-      setPlayers([]);
-      setStats([]);
+      players = [];
     }
   }, [open]);
 
   return (
-    <div>
-      <Autocomplete
-        id="player-search"
-        style={{ width: 400 }}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        isOptionEqualToValue={(option, value) => option.value === value.value}
-        getOptionLabel={option => option.name}
-        options={players}
-        loading={loading}
-        onChange={(ev, value) => {
-          onPlayerSelectHandle(value);
-        }}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label="Player Search"
-            variant="outlined"
-            onChange={ev => {
-              // dont fire API if the user delete or not entered anything
-              if (ev.target.value !== "" || ev.target.value !== null) {
-                onSearchChangeHandle(ev.target.value);
-              }
-            }}
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {loading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              )
-            }}
-          />
-        )}
-      />
-      {<SeasonStats stats={stats} />}
-    </div>
+    <Autocomplete
+      id="player-search"
+      style={{ width: 400 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      isOptionEqualToValue={(option, value) => option.value === value.value}
+      getOptionLabel={option => option.name}
+      options={players}
+      loading={loading}
+      onChange={(ev, value) => {
+        onPlayerSelectHandle(value);
+      }}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="Player Search"
+          variant="outlined"
+          onChange={ev => {
+            // dont fire API if the user delete or not entered anything
+            if (ev.target.value !== "" || ev.target.value !== null) {
+              onSearchChangeHandle(ev.target.value);
+            }
+          }}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            )
+          }}
+        />
+      )}
+    />
   );
 }
